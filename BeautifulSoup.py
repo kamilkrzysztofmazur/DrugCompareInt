d6d012fba76c62c25e403c9870140b0de430b441
Kamil Mazur <kamilkrzysztofmazur@gmail.com>
2020-11-29 17:59:33 +0100

    Zmiana funkcjonalności pętli for na %

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -6,28 +6,25 @@ page = requests.get('https://link.springer.com/article/10.2165/11205830-00000000
 soup = BeautifulSoup(page.content, 'html.parser')
 #print(soup)
 abstract = soup.find(id="Abs1-section").get_text()
-print(abstract)
+#print(abstract)
 #str(abstract)
 #print(abstract.find('Tramadol'))
-
-
 lek1 = 'tramadol'
 lek2 = 'paracetamol'
 choroba1 = 'pain'
-
-
 #if lek1 and lek2 and choroba in abstract:
 #    print('znaleziono podane zmienne')
 #else:
 #    print('nie znaleziono')
-
-
 zdania = abstract.split(". ")
-print(zdania)
+liczba_zdan_negatywnych = 0
 
-for i in zdania:
+for liczba_zdan, i in enumerate(zdania):
     if lek1 and lek2 and choroba1 in i:
-        print('znaleziono podane zmienne w jednym zdaniu --> może zachodzić interakcja. Nalezy się skonsultować z lekarzem lub farmaceutą i zapoznac z artyklem')
+        pass
     else:
-        print('nie znaleziono ich razem w zdaniu')
+        liczba_zdan_negatywnych +=1
+        
+procent = (1-(liczba_zdan_negatywnych/liczba_zdan)) * 100
 
+print(f'Pierwszy grade artykułu {procent} %.')
