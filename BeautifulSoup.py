f1a5d5f2aead3ac51aae493d9a12d5a3136ebc73
Wojciech Snoch <w.snoch@gmail.com>
2020-11-29 12:30:37 +0100

    Wyciaganie abstraktu z artykulu i znajdywanie slow

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,16 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 page = requests.get('https://link.springer.com/article/10.2165/11205830-000000000-00000')
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.find_all('Tramadol paracetamol pain'))
-lek1 = soup.find(id = 'Tramadol')
+#print(soup)
+abstract = soup.find(id="Abs1-section").get_text()
+print(abstract)
+#str(abstract)
+#print(abstract.find('Tramadol'))
 
-lek2 = soup.find(id = 'paracetamol')
 
-choroba = soup.find(id = 'pain')
+lek1 = 'Tramadol'
+lek2 = 'paracetamol'
+choroba = 'pain'
 
-print(lek1)
-print(lek2)
-print(choroba)
+if lek1 and lek2 and choroba in abstract:
+    print('znaleziono podane zmienne')
+else:
+    print('nie znaleziono')
 
