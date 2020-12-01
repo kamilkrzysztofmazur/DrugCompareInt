45510ff7814f7386c6d1e02ffc3a66f14c6c1bb5
Kamil Mazur <kamilkrzysztofmazur@gmail.com>
2020-12-01 14:41:43 +0100

    Poprawiony plik (bazuje na listach i funkcja).

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -2,29 +2,78 @@ import requests
 from bs4 import BeautifulSoup
 
 
-page = requests.get('https://link.springer.com/article/10.2165/11205830-000000000-00000')
-soup = BeautifulSoup(page.content, 'html.parser')
-#print(soup)
-abstract = soup.find(id="Abs1-section").get_text()
-#print(abstract)
-#str(abstract)
-#print(abstract.find('Tramadol'))
 lek1 = 'tramadol'
 lek2 = 'paracetamol'
-choroba1 = 'pain'
-#if lek1 and lek2 and choroba in abstract:
-#    print('znaleziono podane zmienne')
-#else:
-#    print('nie znaleziono')
-zdania = abstract.split(". ")
-liczba_zdan_negatywnych = 0
+choroba = 'pain'
 
-for liczba_zdan, i in enumerate(zdania):
-    if lek1 and lek2 and choroba1 in i:
-        pass
-    else:
-        liczba_zdan_negatywnych +=1
+list_of_article_links = ['https://link.springer.com/article/10.2165/11205830-000000000-00000',
+                         'https://link.springer.com/article/10.2165/11205830-000000000-00000']
+
+
+
+# funkcja sprawdzająca czy w abstrakcie występują trzy słowa kluczowe, 
+def check_abstract(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    abstract = soup.find(id="Abs1-section").get_text() # wybiera text z sekcjii Abs1-section
+    if lek1 and lek2 and choroba in abstract:
+        return url
         
-procent = (1-(liczba_zdan_negatywnych/liczba_zdan)) * 100
 
-print(f'Pierwszy grade artykułu {procent} %.')
+# funkcja sprawdzająca czy w zdaniach abstraku występują trzy słowa kluczowe 
+def check_abstract_sentences(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    abstract = soup.find(id="Abs1-section").get_text() # wybiera text z sekcjii Abs1-section
+    list_of_sentence = abstract.split(". ")
+    for i in list_of_sentence:
+        if lek1 and lek2 and choroba in i:
+            return url
+
+
+list_of_first_loop_reasult = [check_abstract(x) for x in list_of_article_links]
+list_of_second_loop_reasult = [check_abstract_sentences(x) for x in list_of_first_loop_reasult]
+
+
+print(list_of_second_loop_reasult)
+
+# funkcja zwracająca uszczuploną listę linków ( gdy w abstrakcie występują wymienione słowa)
+# list_of_article_links_with_all_words_in_abstract = 
+
+
+
+
+# #if lek1 and lek2 and choroba in abstract:
+# #    print('znaleziono podane zmienne')
+# #else:
+# #    print('nie znaleziono')
+
+# for i in zdania:
+#     if lek1 and lek2 and choroba1 in i:
+#         print('znaleziono podane zmienne w jednym zdaniu --> może zachodzić interakcja. Nalezy się skonsultować z lekarzem lub farmaceutą i zapoznac z artyklem')
+#     else:
+#         print('nie znaleziono ich razem w zdaniu')
+
+
+
+
+
+
+# search = f"{lek1}+{lek2}+{choroba}"
+# print(search)
+# #if lek1 and lek2 and choroba in abstract:
+# #    print('znaleziono podane zmienne')
+# #else:
+# #    print('nie znaleziono')
+# zdania = abstract.split(". ")
+# liczba_zdan_negatywnych = 0
+
+# for liczba_zdan, i in enumerate(zdania):
+#     if lek1 and lek2 and choroba in i:
+#         pass
+#     else:
+#         liczba_zdan_negatywnych +=1
+        
+# procent = (1-(liczba_zdan_negatywnych/liczba_zdan)) * 100
+
+# print(f'Pierwszy grade artykułu {procent} %.')
