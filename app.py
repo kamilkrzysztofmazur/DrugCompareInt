63ed614874b3e1eb281586037d113c64c76395ed
Kamil Mazur <kamilkrzysztofmazur@gmail.com>
2020-12-31 10:07:11 +0100

    strona z trzema listami w wyniku

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from scraping import Filter, LinksGetter
 app = Flask(__name__)
 
 no_interaction_set = {
-'no'}
-'''interaction'}
+'nointeraction'
 'nointeractions',
 'nopain',
 'noadditionalpain',
@@ -17,7 +16,7 @@ no_interaction_set = {
 'nocomplications',
 'notcausesinflammation',
 'notcauseinflammation',
-}'''
+}
 # tu wklejamy clasy i funkcje z baxy dnaych bądź ją na począku importujemy z plki i tlyko wywołujemy
 # tu tez wklejamy kod z BeautifulSoup.py bądź imporujemy clasy z pliku i tlyko wywołujemy zeby program wykonał
 
@@ -43,21 +42,25 @@ def result():
             object.get_list_of_links()
             list_of_objects = [Filter(x) for x in object.list_of_article_links]
             list_of_proper_links = []
+            list_of_proper_links1 = []
+            list_of_proper_links2 = []
             for x in list_of_objects:
                 x.set_of_search_items = set_of_serch_items
                 x.no_interaction_set = no_interaction_set
                 x.get_data_from_page()
             for x in list_of_objects:
                 if x.check_abstract() == True:
+                    list_of_proper_links.append(x.url)
                     if x.check_abstract_sentences() == True:
+                        list_of_proper_links1.append(x.url)
                         if x.check_for_no_interaction_words() == True:
-                            list_of_proper_links.append(x.url)
+                            list_of_proper_links2.append(x.url)
             if not list_of_proper_links:
                 comment = "Nie znaleziono artykułów odpowiadających podanym kryteriom"
             else:
                 comment = "Poniżej artykuły odpowiadające kryteriom wyszukiwania"
 
-            return render_template('result.html', list=list_of_proper_links, comment=comment)  #redirect('/results')
+            return render_template('result.html', list=list_of_proper_links,  list1=list_of_proper_links1,  list2=list_of_proper_links2, comment=comment)  #redirect('/results')
 
         except:
            return 'Błąd wyszukiwania'
