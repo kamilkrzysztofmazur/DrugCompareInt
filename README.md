617894fa2ccd4e68d447929380e3e1c348b71c72
BeataK <beata.kuc1@gmail.com>
2021-01-05 16:23:09 +0100

    Updating documentation

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Drug_Interactions_Comparison
 
-###web-openning_smoke_test
-chromedriver.exe
+### Source
+[Drug Interactions Comparison](http://127.0.0.1:5000/)
+
+### Technologies
+- Python 3.7
+- Pycharm 2020.2.5 or Visual Studio 1.52.1
+- Windows 10 Pro or Linux(Ubuntu) 20.04
+- Git
+
+### Base Technologies
+- [Beautiful Soup 4.9.2](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup)
+- Requests
+- Flask
+- Re
+
+### Tests Technologies
+- PyTest
+- Selenium 141.0
+- [Webdriver.exe 87.0.4280.88](https://sites.google.com/a/chromium.org/chromedriver/downloads) - file download
+
+
+###External Platforms
+[GitHub](https://github.com/kamilkrzysztofmazur/DrugCompareInt) - repository hosting
--- functional_tests.py
+++ functional_tests.py
@@ -2,16 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
 lista = [['paracetamol', 'tramadol', 'pain'],
          ['paracetamol', 'brintellix', 'pain'],
          ['euthyrox', 'brintellix', 'pain'],
-         ['trittico', 'brintellix', 'mental'],
+         ['paracetamol', 'tramadol', 'pain'],
          ['euthyrox', 'pradaxa', 'diabetic']]
-for x in lista:
-    driver = webdriver.Chrome(executable_path='C:\Testfiles\chromedriver.exe')
+
+driver = webdriver.Chrome(executable_path='C:\Testfiles\chromedriver.exe')
+
+for i, x in enumerate(lista):
+    driver.switch_to.window(driver.window_handles[i])
     driver.get('http://127.0.0.1:5000/')
     driver.find_element_by_id("content1").send_keys(x[0])
     driver.find_element_by_id("content2").send_keys(x[1])
     driver.find_element_by_id("content3").send_keys(x[2])
-    driver.find_element(By.TAG_NAME, "button").click()
+    driver.find_element_by_tag_name("button").click()
+    if i+1 < len(lista):
+        driver.execute_script("window.open('');")
+    else:
+        pass
+
+
