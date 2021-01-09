a85f3b2ba0c23727bffa661ffa2394f056711944
BeataK <beata.kuc1@gmail.com>
2021-01-09 21:56:50 +0100

    Aktualizacja pliku BS wg PEP8 - zostal 1 blad z searching_words

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -8,41 +8,44 @@ second_drug = 'tramadol'
 
 disease = 'pain'
 set_of_search_items = {first_drug, second_drug, disease}
-our_search_words = f"{first_drug}+{second_drug}+{disease}"
+searching_words = f"{first_drug}+{second_drug}+{disease}"
 no_interaction_set = {
-    'nointeraction',
-    'nointeractions',
-    'nopain',
-    'noadditional pain',
-    'noserve pain',
-    'noside effects',
+    'no interaction',
+    'no interactions',
+    'no pain',
+    'no additional pain',
+    'no serve pain',
+    'no side effects',
     'harmless',
     'save',
-    'nocomplications',
-    'notcausesinflammation',
-    'notcauseinflammation',
+    'no complications',
+    'not causes inflammation',
+    'not cause inflammation',
     }
 
 
 class LinksGetter:
-    def __init__(self, our_search_words):
-        self.our_search_words = our_search_words
+
+    def __init__(self, searching_words):
+        self.soup = BeautifulSoup(self.link_to_service.content, 'html.parser')
+        self.searching_words = searching_words
         self.link_to_service = None
         self.list_of_article_links = None
 
     def get_page_content(self):
         self.link_to_service = requests.get(f'https://pubmed.ncbi.nlm.nih.gov/?term={searching_words}')
         self.link_to_service.raise_for_status()
-        self.soup = BeautifulSoup(self.link_to_service.content, 'html.parser')
 
     def get_list_of_links(self):
-        content = self.soup.find_all("div", {"class": "docsum-content"})
+        content = self.soup.find_all("div", {"class": "doc sum-content"})
         self.list_of_article_links = ['https://pubmed.ncbi.nlm.nih.gov'+x.a['href'] for x in content]
 
 
 class Filter:
 
     def __init__(self, url):
+        self.list_abstract_sentence = self.abstract.split(". ")
+        self.abstract_regex = re.sub(r"[^a-zA-Z]", " ", self.abstract)
         self.url = url
         self.abstract = None
         self.set_of_abstract_words = None
@@ -51,14 +54,12 @@ class Filter:
     def get_abstract(self):
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
-        self.abstract = soup.find(id="abstract").get_text().lower()  # wybiera text z sekcjii Abs1-section
+        self.abstract = soup.find(id="abstract").get_text().lower()  # selects text from the section Abs1-section
 
     def create_set_of_abstract_words(self):
-        self.abstract_regex = re.sub(r"[^a-zA-Z]", " ", self.abstract)
         self.set_of_abstract_words = set(self.abstract_regex.split(" "))
 
     def create_list_of_sets_of_sentence(self):
-        self.list_abstract_sentence = self.abstract.split(". ")
         self.list_of_abstract_sentence_sets = [set(re.sub(r"[^a-zA-Z]", " ", x).split(" "))
                                                for x in self.list_abstract_sentence]
 
@@ -81,11 +82,11 @@ class Filter:
             if x in self.abstract.replace(' ', ''):
                 return True
 
-obiekt = LinksGetter(searching_words)
-obiekt.get_page_content()
-obiekt.get_list_of_links()
-print(obiekt.list_of_article_links)
-list_of_objects = [Filter(x) for x in obiekt.list_of_article_links]
+object = LinksGetter(searching_words)
+object.get_page_content()
+object.get_list_of_links()
+print(object.list_of_article_links)
+list_of_objects = [Filter(x) for x in object.list_of_article_links]
 list_of_proper_links = []
 for x in list_of_objects:
     x.get_data_from_page()
